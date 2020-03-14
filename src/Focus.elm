a7c8039c9976a038241ab0dc1b8c2638b09c8ae1
Viktor Froberg <vikfroberg@gmail.com>
2020-03-14 23:23:49 +0100

    Fix indention

--- src/Focus.elm
+++ src/Focus.elm
@@ -2,15 +2,15 @@ module Focus exposing (Focus, get, set, update, compose, create)
 
 
 type Focus big small =
-  Focus
-    { get : big -> small
-    , update : (small -> small) -> big -> big
-    }
+    Focus
+        { get : big -> small
+        , update : (small -> small) -> big -> big
+        }
 
 
 create : (big -> small) -> ((small -> small) -> big -> big) -> Focus big small
 create getter updater =
-  Focus { get = getter, update = updater }
+    Focus { get = getter, update = updater }
 
 
 id = 
@@ -19,27 +19,27 @@ id =
 
 get : Focus big small -> big -> small
 get (Focus focus) big =
-  focus.get big
+    focus.get big
 
 
 set : Focus big small -> small -> big -> big
 set (Focus focus) small big =
-  focus.update (always small) big
+    focus.update (always small) big
 
 
 update : Focus big small -> (small -> small) -> big -> big
 update (Focus focus) f big =
-  focus.update f big
+    focus.update f big
 
 
 
 compose : Focus big medium -> Focus medium small -> Focus big small
 compose (Focus largerFocus) (Focus smallerFocus) =
-  let
-    getter big =
-      smallerFocus.get (largerFocus.get big)
+    let
+        getter big =
+            smallerFocus.get (largerFocus.get big)
 
-    updater f big =
-      largerFocus.update (smallerFocus.update f) big
-  in
+        updater f big =
+            largerFocus.update (smallerFocus.update f) big
+    in
     Focus { get = getter, update = updater }
